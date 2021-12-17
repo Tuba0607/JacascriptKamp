241803ce602eb42309e7d97187ff5d1d976e7bc2
Tuba0607 <bogaoglutugba@gmail.com>
2021-12-17 12:47:18 +0300

    initial commit

---
+++ src/data/users.js
@@ -0,0 +1,57 @@
+//id, firstName, lastName, city,age, creditCardNumber
+export const users = [
+    {
+        id: 1,
+        firstName: "Tuğba",
+        lastName: "Boğaoğlu",
+        city:"Antalya",
+        age: 27,
+        creditCardNumber: "123456",
+        type: "customer"
+    },
+    {
+        id: 6,
+        firstName: "Hamza",
+        lastName: "Irmak",
+        city:"Ankara",
+        age: 28,
+        creditCardNumber: "123456",
+        type: "customer"
+    },
+    {
+        id: 2,
+        firstName: "Mustafa",
+        lastName: "Sansar",
+        city: "İstanbul",
+        age: 21,
+        creditCardNumber: "654321",
+        type: "customer"
+    },
+    {
+        id: 3,
+        firstName: "Ayşe",
+        lastName: "Değirmen",
+        city: "İstanbul",
+        salary: 8000,
+        age:20,
+        type: "employee"
+    },
+    {
+        id: 4,
+        firstName: "Bilge",
+        lastName: "Kaya",
+        city: "Bolu",
+        age: 23,
+        salary:17000,
+        type: "employee"
+    },
+    {
+        id: 5,
+        firstName: "Eren",
+        lastName: "Arar",
+        city: "Diyarbakır",
+        age: 20,
+        salary: 15000,
+        type: "employee"
+    }
+]
